a5758b2bb2a54f3b12c6d3b91d5fbf2e1197b92d
Praveen <saipraveenneelisetty27@gmail.com>
2024-08-08 23:30:17 +0530

    Terrform-Multi-Env with tfVars

--- tfvars/r53.tf
+++ tfvars/r53.tf
@@ -4,6 +4,6 @@ resource "aws_route53_record" "expense" {
   ttl             = "1"
   type            = "A"
   records         = each.value
-  zone_id         = var.zone_id
+  zone_id         = startswith(each.key, "frontend") ? [each.value.public_ip] : [each.value.private_ip]
   allow_overwrite = true
 }
