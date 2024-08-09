0141b70a1e0286d2cb89b108f1cd124d18b5ac94
Praveen <saipraveenneelisetty27@gmail.com>
2024-08-09 18:56:40 +0530

    learning aws-workspace

---
+++ workspaces/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "5.58.0"
+        }
+    }
+
+    backend "s3" {
+        bucket = "praveen-remote-state-s3"
+        key = "aws-workspaces"
+        region = "us-east-1"
+        dynamodb_table = "remote-state-s3-locking"
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
